0b4f7ee52ca4db6e5125c5062466d871f471a968
BThomas <75043469+BThomas22tech@users.noreply.github.com>
2022-09-22 13:43:05 -0500

    Button Clicker

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("linked javascript page")
+
+function Logout(element){
+    document.querySelector(".btn").innerText = "Log Out";
+
+}
+
+function hide(element) {
+    element.remove();
+}
